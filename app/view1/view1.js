b36dc5d34eb2acca22a02b1d9bd0a31c8ea50452
NewUser <newuser@ZHANGYUC-NB.local>
2018-01-08 18:05:53 +0800

    -all

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -70,6 +70,14 @@ function View1Ctrl($rootScope, $scope, $http) {
     }
 
 
+    //去重
+    function unique(array) {
+        var n = []; //临时数组
+        for (var i = 0; i < array.length; i++) {
+            if (n.indexOf(array[i]) == -1) n.push(array[i]);
+        }
+        return n;
+    }
 
 //唐姐姐之前写的，现在已经用不上了
     $scope.extractTargetData = function(analysisTitle1) {
@@ -91,11 +99,22 @@ function View1Ctrl($rootScope, $scope, $http) {
         //var analysisResultObject = JSON.parse(analysisTitle1);
         var analysisResultObject = analysisTitle1.data;
         var sentences = analysisResultObject.sentences;
+        var TargetList = [];
+        
+        // var a = [1,2,3,4,5];
+        // var b = a.push(6,7); //a：[-2,-1,1,2,3,4,5]   b：7
+        // console.log(b);
+        // console.log(a);
+
         for (var i = 0; i < sentences.length; i++) {
             var sentence = sentences[i];
             var opinions = sentence.opinions;
             var text = sentence.text;
-            var tokenizedText = sentence.tokenizedText;
+            // var tokenizedText = sentence.tokenizedText;
+            //console.log(opinions)//{category: "DRINKS#PRICES", polarity: "negative", target: "dictumst"}
+            //console.log(text);//sentence
+            // console.log(tokenizedText)
+
             for (var j = 0; j < opinions.length; j++) {
                 var analysisObj = {};
                 var opinion = opinions[j];
@@ -103,10 +122,40 @@ function View1Ctrl($rootScope, $scope, $http) {
                 analysisObj['polarity'] = opinion.polarity;
                 analysisObj['target'] = opinion.target;
                 analysisObj['text'] = text;
-                analysisObj['tokenizedText'] = tokenizedText;
+                // analysisObj['tokenizedText'] = tokenizedText;
                 $scope.analysisArray.push(analysisObj);
+                TargetList.push(analysisObj['target']);
+                
+                //console.log(analysisObj['target']);
+                
+                //console.log(i in analysisObj['target1']);
+
             }
+        
+
+
+
+        //console.log(TargetList);
         }
+        console.log(TargetList); 
+        TargetList = unique(TargetList);
+        console.log(TargetList);
+
+        // var arr = ["apple", "orange", "apple", "orange", "pear", "orange"];
+
+        // function getWordCnt() {
+        //     return arr.reduce(function(prev, next) {
+        //         prev[next] = (prev[next] + 1) || 1;
+        //         return prev;
+        //     }, {});
+        // }
+        // console.log(getWordCnt());
+
+
+
+
+
+
         /*
         for (var i = 0; i < analysisArray.length; ++i) {
             var analysisObj = {};
